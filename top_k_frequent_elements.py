5383c1dafa746c3ec6c0c7717bda7b5b60ea76df
annie.su <annie.su@titansoft.com.sg>
2025-01-16 21:07:50 +0800

    feat: finish Top K Frequent Elements

---
+++ top_k_frequent_elements.py
@@ -0,0 +1,20 @@
+from collections import defaultdict
+from typing import List
+
+
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        dic = defaultdict(int)
+        for i in nums:
+            dic[i] += 1
+        data = list(sorted(dic.items(), key = lambda x : x[1], reverse = True))
+        print(data)
+        result =[x[0] for x in data]
+        return result[0: k]
+
+if __name__ == "__main__":
+    solution = Solution()
+    result = solution.topKFrequent([-1,-1], 1)
+    print(result)
+    result = solution.topKFrequent([1,1,1,2,2,3], 2)
+    print(result)
