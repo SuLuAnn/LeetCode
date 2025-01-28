3bdb1350d2b4e96744d0b19bde508f1417f3c95c
annie.su <annie.su@titansoft.com.sg>
2025-01-28 15:45:31 +0800

    fix: enhance Valid Sudoku

--- valid_sudoku.py
+++ valid_sudoku.py
@@ -3,27 +3,23 @@ from typing import List
 
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        column_store = [[False for i in range(10)] for i in range(10)]
-        row_store = [[False for i in range(10)] for i in range(10)]
-        box_store = [[False for i in range(10)] for i in range(10)]
-        for column in range(len(board)):
-            for row in range(len(board[0])):
+        column_store = [[False] * 10 for _ in range(10)]
+        row_store = [[False] * 10 for _ in range(10)]
+        box_store = [[False] * 10 for _ in range(10)]
+        print(column_store)
+        for column in range(9):
+            for row in range(9):
                 boxes = board[column][row]
                 if boxes == ".":
                     continue
-                print(f"have data : column: {column}, row {row}, boxes {boxes}")
                 index = int(boxes)
                 if column_store[column][index]:
-                    print("c end")
                     return False
                 column_store[column][index] = True
                 if row_store[row][index]:
-                    print("r end")
                     return False
                 row_store[row][index] = True
-                print(f"box: {(column // 3) * 3 + row // 3}")
                 if box_store[(column // 3) * 3 + row // 3][index]:
-                    print("b end")
                     return False
                 box_store[(column // 3) * 3 + row // 3][index] = True
         return True
