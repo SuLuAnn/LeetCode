abb51fe5810dc6cfb7e4dcb35f68ab723c71d3cf
annie.su <annie.su@titansoft.com.sg>
2025-02-20 20:42:09 +0800

    fix: enhance Valid Palindrome

--- valid_palindrome.py
+++ valid_palindrome.py
@@ -5,16 +5,16 @@ class Solution:
         while(left < right):
             left_char = s[left]
             right_char = s[right]
-            if left_char >= 'A' and left_char <= 'Z':
-                left_char = left_char.lower()
-            elif not (left_char >= 'a' and left_char <= 'z') and not (left_char >= '0' and left_char <= '9'):
+            if not left_char.isalnum():
                 left += 1
                 continue
-            if right_char >= 'A' and right_char <= 'Z':
-                right_char = right_char.lower()
-            elif not (right_char >= 'a' and right_char <= 'z') and not (right_char >= '0' and right_char <= '9'):
+            if not right_char.isalnum():
                 right -= 1
                 continue
+            if left_char >= 'A' and left_char <= 'Z':
+                left_char = left_char.lower()
+            if right_char >= 'A' and right_char <= 'Z':
+                right_char = right_char.lower()
             if left_char != right_char:
                 return False
             left += 1
