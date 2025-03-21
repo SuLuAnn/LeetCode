8a8d17e2c63ab09f5bfd8344bf8d3d32a7cbad77
SuLuAnn <a0920086420@gmail.com>
2025-03-21 15:27:16 +0800

    feat: finish Find the Duplicate Number

---
+++ find_the_duplicate_number.py
@@ -0,0 +1,10 @@
+from typing import List
+
+
+class Solution:
+    def findDuplicate(self, nums: List[int]) -> int:
+        records = [0 for _ in range(len(nums))]
+        for num in nums:
+            if records[num] == 1:
+                return num
+            records[num] = 1
