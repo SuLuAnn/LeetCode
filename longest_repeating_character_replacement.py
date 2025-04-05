abd55f8857d46e7f72db31cafdaa9b9114acb5ca
SuLuAnn <a0920086420@gmail.com>
2025-04-05 17:58:42 +0800

    refactor: enhance Longest Repeating Character Replacement

--- longest_repeating_character_replacement.py
+++ longest_repeating_character_replacement.py
@@ -3,12 +3,12 @@ from collections import defaultdict
 
 class Solution:
     def characterReplacement(self, s: str, k: int)  -> int:
-        left, longest_substring = 0, 1
+        left, max_char, longest_substring = 0, 0, 1
         record = defaultdict(int)
         for i, c in enumerate(s):
             record[c] += 1
             cur_length = i - left + 1
-            max_char = max(record.values())
+            max_char = max(max_char, record[c])
             if cur_length > max_char + k:
                 record[s[left]] -= 1
                 left += 1
