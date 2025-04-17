79c23183046bec4e853d6c3d28b00c8abd54be80
SuLuAnn <a0920086420@gmail.com>
2025-04-17 17:11:45 +0800

    feat: enhance Lowest Common Ancestor of a Binary Search Tree

--- lowest_common_ancestor_of_a_binary_search_tree.py
+++ lowest_common_ancestor_of_a_binary_search_tree.py
@@ -7,8 +7,6 @@ class TreeNode:
 
 class Solution:
     def lowestCommonAncestor(self, root: TreeNode, p: TreeNode, q: TreeNode) -> TreeNode:
-        if not root:
-            return None
         max_val, min_val = 0, 0
         if p.val > q.val:
             max_val = p.val
@@ -16,9 +14,12 @@ class Solution:
         else:
             max_val = q.val
             min_val = p.val
+        while root:
+            if root.val > max_val:
+                root = root.left
+            elif root.val < min_val:
+                root = root.right
+            else:
+                return root
         
-        if root.val <= max_val and root.val >= min_val:
-            return root
-        return (self.lowestCommonAncestor(root.left, p, q) or
-        self.lowestCommonAncestor(root.right, p, q))
 
